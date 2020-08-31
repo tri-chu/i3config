5d6852863cf15fd40a1dfffea4cff66610b21da2
Tri Chu <chuminhtri@gmail.com>
2020-08-31 14:41:18 -0500

    clean up setup

---
+++ fuzzy_lock.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+# Take a screenshot
+scrot /tmp/screen_locked.png
+
+# blur  it
+convert /tmp/screen_locked.png -filter Gaussian -resize 10% -define filter:sigma=2.5 -resize 1000% /tmp/screen_locked.png
+
+# Lock screen displaying this image.
+i3lock -i /tmp/screen_locked.png
+
+# Turn the screen off after a delay.
+sleep 60; pgrep i3lock && xset dpms force off

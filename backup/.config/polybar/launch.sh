f26890a38d5c9a871f48a82b82780c37067d83f5
Tri Chu <134897+trichu1988@users.noreply.github.com>
2023-01-31 21:30:29 -0800

    update i3

--- backup/.config/polybar/launch.sh
+++ backup/.config/polybar/launch.sh
@@ -1,20 +1,28 @@
 #!/usr/bin/env bash
 
+killall -q polybar
+
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+      if [ $m == 'HDMI-0' ]
+      then
+        MONITOR=$m polybar --reload bar1 &
+      else
+        MONITOR=$m polybar --reload bar2 &
+      fi
   done
 else
-  polybar --reload example &
+  polybar --reload bar1 &
 fi
 
 # Terminate already running bar instances
-killall -q polybar
+# killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
-polybar bar1 >>/tmp/polybar1.log 2>&1 & disown
+# polybar bar1 >>/tmp/polybar1.log 2>&1 & disown
+# polybar bar2 >>/tmp/polybar2.log 2>&1 & disown
 
 echo "Bars launched..."

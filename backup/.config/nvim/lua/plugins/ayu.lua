182d055ddac00827fbd71fcc2939699e455313c0
Tri Chu <134897+trichu1988@users.noreply.github.com>
2023-02-23 22:45:28 -0800

    add init config for lazy vim

---
+++ backup/.config/nvim/lua/plugins/ayu.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "Shatur/neovim-ayu" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "ayu-mirage",
+    },
+  },
+}
